fd3296bc19482c5024e16715cfe1e25602ad8236
Nicolas Alonso <63936366+n-alonso@users.noreply.github.com>
2023-04-06 12:32:35 +0100

    Update singly_linked_list.js

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -2,62 +2,62 @@ const Node = require('./Node');
 
 class LinkedList {
   constructor() {
-    this.head = null;
-    this.length = 0;
+    #head = null;
+    #length = 0;
   }
 
   addToHead(data) {
     const newHead = new Node(data);
-    const currentHead = this.head;
-    this.head = newHead;
+    const currentHead = this.#head;
+    this.#head = newHead;
     if (currentHead) {
-      this.head.setNextNode(currentHead);
+      this.#head.setNextNode(currentHead);
     }
-    this.length++;
+    this.#length++;
   }
 
   addToTail(data) {
-    let tail = this.head;
+    let tail = this.#head;
     if (!tail) {
-      this.head = new Node(data);
+      this.#head = new Node(data);
     } else {
       while (tail.getNextNode() !== null) {
         tail = tail.getNextNode();
       }
       tail.setNextNode(new Node(data));
     }
-    this.length++;
+    this.#length++;
   }
 
   removeHead() {
-    const removedHead = this.head;
+    const removedHead = this.#head;
     if (!removedHead) {
       return;
     }
-    this.head = removedHead.getNextNode();
-    this.length--;
-    return removedHead.data;
+    this.#head = removedHead.getNextNode();
+    this.#length--;
+    return removedHead.getData();
   }
   
   getSize() {
-    console.log(`length: ${this.length}`);
-    return this.length;
+    console.log(`length: ${this.#length}`);
+    return this.#length;
   }
   
   find(value) {
-    let currentNode = this.head;
+    let currentNode = this.#head;
     while (currentNode) {
-      if (currentNode.data === value) return currentNode;
+      if (currentNode.getData() === value) return currentNode;
       currentNode = currentNode.getNextNode();
     }
     return null;
   }
 
   printList() {
-    let currentNode = this.head;
+    let currentNode = this.#head;
     let output = '<head> ';
     while (currentNode !== null) {
-      output += currentNode.data + ', ';
+      output += currentNode.getData() + ', ';
       currentNode = currentNode.getNextNode();
     }
     output += '<tail>';
