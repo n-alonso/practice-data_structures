d28d460b9f33740cc18cfa6d04e32070f6acd7ea
Nicolas Alonso <63936366+n-alonso@users.noreply.github.com>
2023-04-08 22:38:24 +0100

    Update nodes.js

--- nodes.js
+++ nodes.js
@@ -21,6 +21,41 @@ class Node {
   }
 }
 
+
+// DOUBLY LINKED NODE
+
+class DoublyLinkedNode {
+  constructor(data) {
+    this.#data = data
+    this.#next = null
+    this.#previous = null
+  }
+  
+  getNextNode() {
+    return this.#next
+  }
+  
+  getPreviousNode() {
+    return this.#previous
+  }
+  
+  setNextNode(node) {
+    if (node instanceof Node || node === null) {
+      this.#next = node;
+    } else {
+      throw new Error('Next node must be an instance of the Node class.');
+    }
+  }
+  
+  setPreviousNode(node) {
+    if (node instanceof Node || node === null) {
+      this.#previous = node;
+    } else {
+      throw new Error('Previous node must be an instance of the Node class.');
+    }
+  }
+}
+
 const strawberryNode = new Node('Berry Tasty')
 const vanillaNode = new Node('Vanilla')
 const coconutNode = new Node('Coconuts for Coconut')
